896f5a606a839a1e14115faaa20fb00e5f099ca2
milena-j <milenajocic.work@gmail.com>
2024-01-25 12:32:01 +0100

    struttura

--- linkedin-clone/src/pages/ProfilePage.jsx
+++ linkedin-clone/src/pages/ProfilePage.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
+import { SideBarComponent } from '../components/SideBarComponent'
+import { MainProfileComponent } from '../components/MainProfileComponent'
 
 export default function ProfilePage() {
   return (
     <Container>
       <Row className='gap-1 justify-content-around'>
-        <Col xs={12} md={8}  className='bg-body-tertiary'>
-          <p>Contenuto principale</p>
+        <Col xs={12} md={8} className='bg-body-tertiary'>
+          <MainProfileComponent />
         </Col>
         <Col md={3} className='bg-body-tertiary'>
-          <p>Barra laterale</p>
+          <SideBarComponent />
         </Col>
       </Row>
     </Container>
