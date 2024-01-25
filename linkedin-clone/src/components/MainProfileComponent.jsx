92b49c070c8bef51424c4531ccbffe2a2b166196
milena-j <milenajocic.work@gmail.com>
2024-01-25 17:31:36 +0100

    sezioni analisi e risorse

--- linkedin-clone/src/components/MainProfileComponent.jsx
+++ linkedin-clone/src/components/MainProfileComponent.jsx
@@ -1,32 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { ProfileInfoComponent } from './ProfileInfoComponent';
+import ProfileAnalisiComponent from './ProfileAnalisiComponent'
+import ProfileRisorseComponent from './ProfileRisorseComponent'
 import ProfileEsperienzaComponent from './ProfileEsperienzaComponent';
 
 export const MainProfileComponent = () => {
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIyMmU3OTkxM2Y2NTAwMThkMDk1YmEiLCJpYXQiOjE3MDYxNzYxMjEsImV4cCI6MTcwNzM4NTcyMX0.O1zhA65zNqI-ZmpFBTPAmpGJ-zFueo8cw4ei9XuHWXw';
 
-  const[profile, setProfile] = useState([]);
+  const [profile, setProfile] = useState([]);
 
-  useEffect(()=>{
-    fetch("https://striveschool-api.herokuapp.com/api/profile/",{
+  useEffect(() => {
+    fetch("https://striveschool-api.herokuapp.com/api/profile/", {
       method: 'GET',
-      headers:{
+      headers: {
         'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json',
       }
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data[0]);
-      setProfile(data[34]);
-    })
-    .catch(error=>{
-      console.error(error)
-    })
-  },[])
+      .then(response => response.json())
+      .then(data => {
+        console.log(data[0]);
+        setProfile(data[34]);
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
   return (
     <>
       <ProfileInfoComponent profile={profile} />
+      <ProfileAnalisiComponent />
+      <ProfileRisorseComponent/>
       <ProfileEsperienzaComponent />
     </>
   );
