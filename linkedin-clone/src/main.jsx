ac106083427af7f41b358b3bbb2b0ee705225a80
Francesco Zatti <francesco.zatti98gmail.com>
2024-01-25 16:28:11 +0100

    Co-authored-by: systemError998 <systemError998@users.noreply.github.com>

--- linkedin-clone/src/main.jsx
+++ linkedin-clone/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './redux/store'
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
