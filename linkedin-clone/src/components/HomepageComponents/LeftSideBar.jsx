e4111efddf5eca52854d68babab33334ba5367b3
Giulius94 <giuliuscb@protonmail.com>
2024-01-26 16:14:23 +0100

    Scheletro Homepage

---
+++ linkedin-clone/src/components/HomepageComponents/LeftSideBar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Button, Card, ListGroup } from 'react-bootstrap'
+
+export default function LeftSideBar() {
+    return (
+        <>
+            <Card className='w-75'>
+                <div class='position-relative'>
+                    <Card.Img variant="top" className='imgLeftSideBar' src="https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg" />
+                    <img src='https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg' alt="profile-img" style={{ height: "5em", width: "5em" }}
+                        className='rounded-circle position-absolute top-100 start-50 translate-middle border border-white border-3'
+                    />
+                </div>
+                <Card.Body>
+                    <Card.Title className='mt-5 text-center'>Gastani Frizzi</Card.Title>
+                    <Card.Text className='text-center text-secondary mt-1'>
+                        Studente presso boh
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className='list-group-flush'>
+                    <ListGroup.Item variant='ligth d-flex justify-content-between align-items-center flex-wrap'>
+                        <Button variant='nav-link outline-secondary' className='text-secondary fs-6'>Visitatori del Profilo</Button><span className='text-primary fw-bold'>19</span>
+                        <Button variant='nav-link outline-secondary' className='text-secondary fs-6'>Impressioni del Post</Button><span className='text-primary fw-bold'>33</span>
+                    </ListGroup.Item>
+                    <ListGroup.Item action variant='ligth d-flex justify-content-between align-items-center flex-wrap'>
+                        <p className='m-0 text-secondary'>Cerca lavoro in modo più smart con Premium</p>
+                        <a href="#" className='text-decoration-none text-black fw-bold'><i className="bi bi-square-fill text-warning me-2"></i>Prova per 0€</a>
+                    </ListGroup.Item>
+                    <ListGroup.Item action variant='ligth d-flex justify-content-between align-items-center flex-wrap'>
+                        <p className='m-0 text-secondary fw-bold'><i className="bi bi-bookmark-fill me-2 fw-bold"></i>I mie elementi</p>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
+            <Card className='w-75 mt-2'>
+                <Card.Body>
+                
+                </Card.Body>
+                <ListGroup className='list-group-flush'>
+                    <ListGroup.Item action variant='ligth'>
+                        <p className='text-center m-1 fw-bold text-secondary'>Scropri di più</p>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </>
+    )
+}
