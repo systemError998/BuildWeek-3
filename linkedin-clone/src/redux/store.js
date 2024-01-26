e50f9d728a5d0c9d0f168f3b47ca6ef4dff50c83
systemError998 <annacerasoli998@gmail.com>
2024-01-26 14:09:48 +0100

    correzione errori

--- linkedin-clone/src/redux/store.js
+++ linkedin-clone/src/redux/store.js
@@ -1,10 +1,4 @@
-
 import {configureStore} from '@reduxjs/toolkit'
-
-export const store = configureStore({
-    reducer : {
-        
-import {configureStore} from '@reduxjs/toolkit';
 import SidebarSliceReducer from './slice/SidebarSlice'
 
 export const store = configureStore({
