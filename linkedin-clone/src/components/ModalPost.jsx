91f61e42c61955eb50277b6e1d685e897b8ce3da
Francesco Zatti <francesco.zatti98gmail.com>
2024-01-30 13:03:38 +0100

    make toggleino

--- linkedin-clone/src/components/ModalPost.jsx
+++ linkedin-clone/src/components/ModalPost.jsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchNavUser } from '../redux/slice/NavUserSlice';
+import { useState, useEffect } from "react";
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchNavUser } from "../redux/slice/NavUserSlice";
 
 function ModalPost({ handleCloseModal, show }) {
-
   const dispatch = useDispatch();
 
   const userFetch = useSelector((state) => state.navUser.navUser);
@@ -15,62 +14,142 @@ function ModalPost({ handleCloseModal, show }) {
     dispatch(fetchNavUser());
   }, []);
 
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-
   return (
     <>
-      {userFetch &&
+      {userFetch && (
         <Modal show={show} onHide={handleCloseModal}>
-          <Modal.Header closeButton>
-            <Modal.Title className='d-flex gap-2'>
+          <Modal.Header
+            className="border-0 d-flex justify-content-between align-items-top"
+            closeButton
+          >
+            <Modal.Title className="d-flex gap-2">
               <div>
                 <img
-                  className='rounded-circle'
+                  className="rounded-circle"
                   src={userFetch.image}
                   alt=""
-                  style={{ width: '50px', height: '50px' }} />
+                  style={{ width: "50px", height: "50px" }}
+                />
               </div>
               <div>
-                <h5>{userFetch.name} {userFetch.surname}</h5>
-                <h6>Pubblica: <span>Solo veri amici</span></h6>
+                <h5>
+                  {userFetch.name} {userFetch.surname}
+                </h5>
+                <h6>
+                  Pubblica: <span>Solo veri amici</span>
+                </h6>
               </div>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
               <Form.Control
-                placeholder='Di cosa vorresti parlare?'
+                placeholder="Di cosa vorresti parlare?"
                 onChange={handleInputChange}
                 as="textarea"
                 rows={3}
-                value={inputValue} />
+                value={inputValue}
+                className="border-0 outline-0"
+              />
             </Form.Group>
+            <Modal.Footer className="border-0 ms-0 ps-0 justify-content-start">
+              <div className="d-flex">
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Aggiungi contenuto multimediale</Tooltip>}
+                >
+                  <Button className="modalPostBtn me-2">
+                    <i class="modalBtnIcon bi bi-image"></i>
+                  </Button>
+                </OverlayTrigger>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Crea un evento</Tooltip>}
+                >
+                  <Button className="modalPostBtn me-2">
+                    <i class="modalBtnIcon bi bi-calendar3"></i>
+                  </Button>
+                </OverlayTrigger>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Festeggia un'occasione speciale</Tooltip>}
+                >
+                  <Button className="modalPostBtn me-2">
+                    <i class="modalBtnIcon bi bi-stars"></i>
+                  </Button>
+                </OverlayTrigger>
+
+                {/*bottoncino input "Make Toggleino"*/}
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Altro</Tooltip>}
+                >
+                  <Button className="modalPostBtn me-2">
+                    <i className="modalBtnIcon bi bi-three-dots"></i>
+                  </Button>
+                </OverlayTrigger>
+
+                {/*bottoncini da nascondere*/}
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Altro</Tooltip>}
+                >
+                  <Button className="modalPostBtn me-2">
+                    <i class="modalBtnIcon bi bi-briefcase-fill"></i>
+                  </Button>
+                </OverlayTrigger>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Altro</Tooltip>}
+                >
+                  <Button className="modalPostBtn me-2">
+                    <i class="modalBtnIcon bi bi-bar-chart-line-fill"></i>
+                  </Button>
+                </OverlayTrigger>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Altro</Tooltip>}
+                >
+                  <Button className="modalPostBtn me-2">
+                    <i class="modalBtnIcon bi bi-file-earmark-text-fill"></i>
+                  </Button>
+                </OverlayTrigger>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Altro</Tooltip>}
+                >
+                  <Button className="modalPostBtn me-2">
+                    <i class="modalBtnIcon bi bi-file-person-fill"></i>
+                  </Button>
+                </OverlayTrigger>
+              </div>
+            </Modal.Footer>
           </Modal.Body>
           <Modal.Footer>
-            <div>
-              <Button className='btn-secondary text-tertiary'>
-                <svg className="text-tertiary" role="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" data-supported-dps="24x24" data-test-icon="starburst-medium">
-                  <use href="#starburst-medium" width="24" height="24"></use>
-                </svg>
-              </Button>
-              <Button> </Button>
-              <Button> </Button>
-              <Button> </Button>
-            </div>
-            <div>
-              <i class="bi bi-clock"></i>
+            <div className="d-flex gap-2 align-items-center">
+              <i className="bi bi-clock fs-5 m-0 p-0"></i>
               <Button
-                className={inputValue === '' ? "bg-secondary disabled border-0 rounded-pill" : "bg-primary border-0 rounded-pill"}
-                onClick={handleCloseModal}>
+                className={
+                  inputValue === ""
+                    ? "bg-secondary disabled border-0 rounded-pill"
+                    : "bg-primary border-0 rounded-pill"
+                }
+                onClick={handleCloseModal}
+              >
                 Pubblica
               </Button>
             </div>
           </Modal.Footer>
-        </Modal>}
+        </Modal>
+      )}
     </>
   );
 }
